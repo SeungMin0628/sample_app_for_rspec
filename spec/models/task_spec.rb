65fd52cff17b5ac756812fcf95a33a7cddd34da7
SeungMin0628 <smlee95kr@gmail.com>
2019-11-29 21:17:47 +0900

    [Add] write task model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#save" do
+    let(:task) { build(:task) }
+    subject { task.save }
+
+    context "when title and status are exist" do
+      it { is_expected.to be_truthy }
+    end
+
+    context "when title is blank" do
+      it "is not valid" do
+        task.title = ''
+        is_expected.to be_falsey
+      end
+    end
+
+    context "when title is overlap with other title" do
+      it "is not valid" do
+        other_task = create :task
+        task.title = other_task.title
+        is_expected.to be_falsey
+      end
+    end
+
+    context "when status is null" do
+      it "is not valid" do
+        task.status = nil
+        is_expected.to be_falsey
+      end
+    end
+    
+    describe "relations" do
+      specify 'Tasks belongs to user' do
+        task.user = nil
+        is_expected.to be_falsey
+      end
+    end
+  end
 end
