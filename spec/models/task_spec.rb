670abfd1a9497f266b74f6073a342d3c1bc8bcb6
SeungMin0628 <smlee95kr@gmail.com>
2019-11-30 13:04:19 +0900

    [Modify] refactor task rspec code

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,39 +2,57 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe "#save" do
-    let(:task) { build(:task) }
+    let(:user) { create :user }
+    let(:task) { user.tasks.new(title: title, status: status) }
     subject { task.save }
 
+    # About valid datas
+    shared_context 'valid datas' do
+      let(:title) { 'TEST EXAMPLE TITLE' }
+      let(:status) { %i[todo doing done].sample }
+    end
+
     context "when title and status are exist" do
-      it { is_expected.to be_truthy }
+      include_context 'valid datas'
+      it { expect(task).to be_valid }
+    end
+
+    # About title
+    shared_examples 'title is invalid' do
+      it do
+        expect(task).not_to be_valid
+        expect(task.errors[:title]).to be_present
+      end
     end
 
     context "when title is blank" do
-      it "is not valid" do
-        task.title = ''
-        is_expected.to be_falsey
-      end
+      include_context 'valid datas'
+      let(:title) { nil }
+      it_behaves_like 'title is invalid'
     end
 
     context "when title is overlap with other title" do
-      it "is not valid" do
-        other_task = create :task
-        task.title = other_task.title
-        is_expected.to be_falsey
-      end
+      include_context 'valid datas'
+      let(:title) { create(:task).title }
+      it_behaves_like 'title is invalid'
     end
 
+    # About status
     context "when status is null" do
+      include_context 'valid datas'
+      let(:status) { nil }
       it "is not valid" do
-        task.status = nil
-        is_expected.to be_falsey
+        expect(task).not_to be_valid
+        expect(task.errors[:status]).to be_present
       end
     end
     
     describe "relations" do
+      include_context 'valid datas'
       specify 'Tasks belongs to user' do
         task.user = nil
-        is_expected.to be_falsey
+        expect(task).not_to be_valid
+        expect(task.errors[:user]).to be_present
       end
     end
   end
